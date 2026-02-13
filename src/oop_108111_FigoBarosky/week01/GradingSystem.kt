8f4923cf99f8d68bc5f4ddd622d25f8de1d24b8d
Figo Barosky <figobarosky1@gmail.com>
2026-02-13 20:42:10 +0700

    week01: initial setup and procedural code

---
+++ src/oop_108111_FigoBarosky/week01/GradingSystem.kt
@@ -0,0 +1,9 @@
+package oop_108111_FigoBarosky.week01
+
+fun main() {
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    // Concatenation gaya lama
+    println("Nama: " + name + ", Nilai: " + score)
+}
