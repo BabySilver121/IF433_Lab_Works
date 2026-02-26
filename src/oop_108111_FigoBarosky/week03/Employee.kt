7cdb81dc040e8813591507ed4c840a5b2844b9f4
Figo Barosky <figobarosky1@gmail.com>
2026-02-26 23:18:44 +0700

    week03: add private property

--- src/oop_108111_FigoBarosky/week03/Employee.kt
+++ src/oop_108111_FigoBarosky/week03/Employee.kt
@@ -1,6 +1,7 @@
 package oop_108111_FigoBarosky.week03
 
 class Employee(val name: String) {
+
     var salary: Int = 0
         set(value) {
             if (value < 0) {
@@ -10,4 +11,18 @@ class Employee(val name: String) {
                 field = value
             }
         }
+
+    // PRIVATE PROPERTY
+    private var performanceRating: Int = 3
+
+    // FUNCTION untuk ubah rating
+    fun increasePerformance() {
+        performanceRating++
+        println("Kinerja $name meningkat! Rating: $performanceRating")
+    }
+
+    // FUNCTION untuk lihat status
+    fun printStatus() {
+        println("Karyawan: $name, Rating: $performanceRating")
+    }
 }
