26cd76083363bfe4993dc2594d115fafcdfc7653
Figo Barosky <figobarosky1@gmail.com>
2026-02-26 23:30:27 +0700

    week03: (task 1) create Weapon class with validation

--- src/oop_108111_FigoBarosky/week03/Weapon.kt
+++ src/oop_108111_FigoBarosky/week03/Weapon.kt
@@ -1,4 +1,29 @@
 package oop_108111_FigoBarosky.week03
 
-class Weapon {
+class Weapon(
+    val name: String, // Read-only
+    damage: Int
+) {
+
+    var damage: Int = damage
+        set(value) {
+            if (value < 0) {
+                println("Damage tidak boleh negatif!")
+                return
+            }
+
+            if (value > 1000) {
+                println("Damage tidak boleh lebih dari 1000! Diset ke 1000.")
+                field = 1000
+            } else {
+                field = value
+            }
+        }
+
+    val tier: String
+        get() = when {
+            damage > 800 -> "Legendary"
+            damage > 500 -> "Epic"
+            else -> "Common"
+        }
 }
