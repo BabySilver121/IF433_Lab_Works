bfa34ba2acc61045dddbd8d6dc667417c0a98b34
Figo Barosky <figobarosky1@gmail.com>
2026-02-26 23:37:13 +0700

    week03: (task 2) implement Player testing in main

--- src/oop_108111_FigoBarosky/week03/Main.kt
+++ src/oop_108111_FigoBarosky/week03/Main.kt
@@ -1,6 +1,7 @@
 package oop_108111_FigoBarosky.week03
 
 fun main() {
+
     val e = Employee("Budi")
 
     // 1. Test Salary
@@ -15,15 +16,27 @@ fun main() {
     // 3. Test Computed Property
     println("Pajak yang harus dibayar: ${e.tax}")
 
-    val weapon = Weapon("Excalibur", 100)
 
     println("=== Testing Weapon ===")
+    val weapon = Weapon("Excalibur", 100)
 
-    weapon.damage = -50      // Harus gagal
+    weapon.damage = -50
     println("Damage sekarang: ${weapon.damage}")
 
-    weapon.damage = 9999     // Harus jadi 1000
+    weapon.damage = 9999
     println("Damage sekarang: ${weapon.damage}")
-
     println("Tier: ${weapon.tier}")
+
+
+    println("\n=== Testing Player ===")
+
+    val player = Player("Figo")
+
+    // println(player.xp) // ERROR (karena private)
+
+    player.addXp(50)   // Masih level 1
+    println("Level sekarang: ${player.level}")
+
+    player.addXp(60)   // Total 110 → Level 2
+    println("Level sekarang: ${player.level}")
 }
