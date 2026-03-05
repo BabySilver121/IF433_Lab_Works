24dfda9b3dd87a055775722fa585d7bb81fce7b8
Figo Barosky <figobarosky1@gmail.com>
2026-03-05 17:36:44 +0700

    week04: (task 2) test Employee hierarchy in main

--- src/oop_108111_FigoBarosky/week04/Main.kt
+++ src/oop_108111_FigoBarosky/week04/Main.kt
@@ -19,4 +19,14 @@ fun main() {
     myElectricCar.honk()
     myElectricCar.openTrunk()
 
+    println("\n--- Testing Employee Hierarchy ---")
+
+    val manager = Manager("Budi", 10000000)
+    manager.work()
+    println("Bonus Manager: Rp ${manager.calculateBonus()}")
+
+    val developer = Developer("Andi", 8000000, "Kotlin")
+    developer.work()
+    println("Bonus Developer: Rp ${developer.calculateBonus()}")
+
 }
