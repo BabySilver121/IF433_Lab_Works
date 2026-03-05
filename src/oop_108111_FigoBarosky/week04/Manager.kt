879c67adf8719160efe2cf8b4e355f2125628ba8
Figo Barosky <figobarosky1@gmail.com>
2026-03-05 17:20:17 +0700

    week04: (task 2) create Manager class overriding Employee

---
+++ src/oop_108111_FigoBarosky/week04/Manager.kt
@@ -0,0 +1,13 @@
+package oop_108111_FigoBarosky.week04
+
+class Manager(name: String, baseSalary: Int) : Employee(name, baseSalary) {
+
+    override fun work() {
+        println("$name sedang memimpin rapat divisi.")
+    }
+
+    override fun calculateBonus(): Int {
+        return super.calculateBonus() + 50000
+    }
+
+}
