e492b83da965daed1b7cc8702e628348cd37a203
Figo Barosky <figobarosky1@gmail.com>
2026-03-05 17:09:07 +0700

    week04: (task 1) create ElectricCar class with final override

---
+++ src/oop_108111_FigoBarosky/week04/ElectricCar.kt
@@ -0,0 +1,13 @@
+package oop_108111_FigoBarosky.week04
+
+class ElectricCar(
+    brand: String,
+    numberOfDoors: Int,
+    val batteryCapacity: Int
+) : Car(brand, numberOfDoors) {
+
+    final override fun accelerate() {
+        println("$brand berakselerasi dalam sunyi. Kapasitas baterai: $batteryCapacity%.")
+    }
+
+}
