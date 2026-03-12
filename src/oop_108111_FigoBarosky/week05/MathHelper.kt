f8f14093dfe23f03c9ed1289d08cbcf3e23f9bed
Figo Barosky <figobarosky1@gmail.com>
2026-03-12 21:28:38 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_108111_FigoBarosky/week05/MathHelper.kt
@@ -0,0 +1,13 @@
+package oop_108111_FigoBarosky.week05
+
+class MathHelper {
+
+    // Luas persegi
+    fun hitungLuas(sisi: Int): Int {
+        return sisi * sisi
+    }
+
+    // Luas persegi panjang
+    fun hitungLuas(panjang: Int, lebar: Int): Int {
+        return panjang * lebar
+    }
