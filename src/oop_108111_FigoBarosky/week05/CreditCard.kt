4a6f667b6595354dc8f48b97077fcd2b385cfcb2
Figo Barosky <figobarosky1@gmail.com>
2026-03-12 21:38:32 +0700

    week05: (task 2) create CreditCard class

---
+++ src/oop_108111_FigoBarosky/week05/CreditCard.kt
@@ -0,0 +1,17 @@
+package oop_108111_FigoBarosky.week05
+
+class CreditCard(accountName: String, val limit: Double) : PaymentMethod(accountName) {
+
+    var usedAmount: Double = 0.0
+
+    override fun processPayment(amount: Double) {
+
+        if (usedAmount + amount <= limit) {
+            usedAmount += amount
+            println("Transaksi credit card berhasil. Total terpakai: $usedAmount")
+        } else {
+            println("Transaksi ditolak. Melebihi limit kartu.")
+        }
+
+    }
+}
