ca1b4a64be93012cbc5bf3b8efc66f92dad60a66
Figo Barosky <figobarosky1@gmail.com>
2026-03-19 21:02:27 +0700

    week06: (task) create SmartSpeaker

---
+++ src/oop_108111_FigoBarosky/week06/SmartSpeaker.kt
@@ -0,0 +1,19 @@
+package oop_108111_FigoBarosky.week06
+
+class SmartSpeaker(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable {
+
+    override fun turnOn() {
+        println("$name menyala.")
+    }
+
+    override fun turnOff() {
+        println("$name mati.")
+    }
+
+    fun playMusic(song: String) {
+        println("Memutar lagu $song dari Spotify.")
+    }
+}
