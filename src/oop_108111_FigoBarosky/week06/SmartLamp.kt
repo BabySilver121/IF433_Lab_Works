b71eacb7fe6ad2cc56a0a34d22a1565dcef24fbf
Figo Barosky <figobarosky1@gmail.com>
2026-03-19 21:01:45 +0700

    week06: (task) create SmartLamp

---
+++ src/oop_108111_FigoBarosky/week06/SmartLamp.kt
@@ -0,0 +1,15 @@
+package oop_108111_FigoBarosky.week06
+
+class SmartLamp(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable {
+
+    override fun turnOn() {
+        println("$name menyala.")
+    }
+
+    override fun turnOff() {
+        println("$name mati.")
+    }
+}
