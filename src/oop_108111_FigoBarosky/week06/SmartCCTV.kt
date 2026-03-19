837a22435f156d5c574f72d24e6f201ef92161a2
Figo Barosky <figobarosky1@gmail.com>
2026-03-19 21:03:07 +0700

    week06: (task) create SmartCCTV with compound interfaces

---
+++ src/oop_108111_FigoBarosky/week06/SmartCCTV.kt
@@ -0,0 +1,20 @@
+package oop_108111_FigoBarosky.week06
+
+class SmartCCTV(
+    override val id: String,
+    override val name: String
+) : SmartDevice, Switchable, Recordable {
+
+    override fun turnOn() {
+        println("$name menyala.")
+        startRecord()
+    }
+
+    override fun turnOff() {
+        println("$name mati.")
+    }
+
+    override fun startRecord() {
+        println("$name mulai merekam.")
+    }
+}
