606e9d378491e6eaae1091fbb9699971a5c6287a
Figo Barosky <figobarosky1@gmail.com>
2026-04-23 22:46:20 +0700

    week08: (task) create sealed class Product and data classes

---
+++ src/oop_108111_FigoBarosky/week08/EcommerceModels.kt
@@ -0,0 +1,7 @@
+package oop_108111_FigoBarosky.week08
+
+sealed class Product
+
+data class Electronic(val id: String, val name: String, val warrantyMonths: Int) : Product()
+
+data class Clothing(val id: String, val name: String, val size: String) : Product()
