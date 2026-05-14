49dccc780ea39ab9fb1fa4bfbaa3ccfebe82d6bf
Figo Barosky <figobarosky1@gmail.com>
2026-05-15 03:33:12 +0700

    week10: (task) wrap repository data into ApiResponse

--- src/oop_108111_FigoBarosky/week10/CryptoDashboard.kt
+++ src/oop_108111_FigoBarosky/week10/CryptoDashboard.kt
@@ -7,4 +7,9 @@ fun main() {
     coinRepo.add(Coin("BTC", 1.5))
     coinRepo.add(Coin("ETH", 8.0))
     coinRepo.add(Coin("USDT", 2500.0))
+
+    val response = ApiResponse(
+        "200 OK",
+        coinRepo.getAll()
+    )
 }
