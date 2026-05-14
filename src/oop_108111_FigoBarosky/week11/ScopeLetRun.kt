0cf151d1fb585bd08337da47facd0fa7cce0e565
Figo Barosky <figobarosky1@gmail.com>
2026-05-15 03:42:24 +0700

    week11: implement let function for null safety

---
+++ src/oop_108111_FigoBarosky/week11/ScopeLetRun.kt
@@ -0,0 +1,14 @@
+package oop_108111_FigoBarosky.week11
+
+fun main() {
+    println("=== TEST LET FUNCTION ===")
+    val name: String? = "Alexander"
+
+    // Mengeksekusi blok hanya jika objek tidak null
+    val length = name?.let {
+        println("Nama terdeteksi: $it") // 'it' merujuk pada name
+        it.length // Mengembalikan nilai expression terakhir
+    }
+
+    println("Panjang nama: $length")
+}
