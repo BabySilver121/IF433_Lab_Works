31415251323925ef43d56e9668c362b0e393df71
Figo Barosky <figobarosky1@gmail.com>
2026-05-28 20:44:02 +0700

    week13: (task) test full crypto trade logger pipeline

--- src/oop_108111_FigoBarosky/week13/CryptoTradeLogger.kt
+++ src/oop_108111_FigoBarosky/week13/CryptoTradeLogger.kt
@@ -83,4 +83,11 @@ fun main() {
     val loadedData = loadTrades("crypto_trades.csv")
 
     val totalPnl = loadedData.sumOf { it.pnl }
+
+    loadedData.forEach {
+
+        println(it)
+    }
+
+    println("==== TOTAL PnL BERSIH: $totalPnl ====")
 }
