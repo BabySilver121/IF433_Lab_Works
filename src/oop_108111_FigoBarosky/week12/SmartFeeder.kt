aa193ae675a9b53fb2d9a61782b2bdbfde0ffb27
Figo Barosky <figobarosky1@gmail.com>
2026-05-28 17:37:46 +0700

    week12: (task) handle feeding execution with multiple catch

--- src/oop_108111_FigoBarosky/week12/SmartFeeder.kt
+++ src/oop_108111_FigoBarosky/week12/SmartFeeder.kt
@@ -26,4 +26,25 @@ fun dispenseKibble(
 fun main() {
 
     var currentKibbleStock = 50
+
+    try {
+
+        currentKibbleStock = dispenseKibble(
+            requestedGram = 80,
+            availableGram = currentKibbleStock,
+            isJammed = false
+        )
+
+    } catch (e: DispenserJamException) {
+
+        println("Error dispenser: ${e.message}")
+
+    } catch (e: FoodEmptyException) {
+
+        println("Error stok makanan: ${e.message}")
+
+    } catch (e: Exception) {
+
+        println("Terjadi kesalahan umum.")
+    }
 }
