9d3623043c5ef04751f41f52be03b0a393cf2806
Figo Barosky <figobarosky1@gmail.com>
2026-05-28 20:38:39 +0700

    week13: create Student model

---
+++ src/oop_108111_FigoBarosky/week13/StudentSerialization.kt
@@ -0,0 +1,10 @@
+package oop_108111_FigoBarosky.week13
+
+import java.io.File
+import java.io.FileNotFoundException
+
+data class Student(
+    val name: String,
+    val age: Int,
+    val gpa: Double
+)
