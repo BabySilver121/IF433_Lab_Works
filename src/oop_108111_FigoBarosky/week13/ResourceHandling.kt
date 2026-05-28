d7f3a8a2204adc33163c30758933b0f81dab67d4
Figo Barosky <figobarosky1@gmail.com>
2026-05-28 20:37:14 +0700

    week13: implement manual close for file stream

---
+++ src/oop_108111_FigoBarosky/week13/ResourceHandling.kt
@@ -0,0 +1,21 @@
+package oop_108111_FigoBarosky.week13
+
+import java.io.File
+
+fun main() {
+
+    println("=== TEST UNSAFE RESOURCE HANDLING ===")
+
+    val unsafeFile = File("unsafe-logs.txt")
+
+    // Membuka stream secara manual
+    val writer = unsafeFile.printWriter()
+
+    writer.println("Log 1: Membuka koneksi database...")
+    writer.println("Log 2: Menulis data pengguna...")
+
+    // Wajib dipanggil secara manual jika tidak memakai blok 'use'
+    writer.close()
+
+    println("Proses penulisan unsafe selesai.")
+}
