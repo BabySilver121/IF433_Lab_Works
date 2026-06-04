195f8f09b6997f7af5bb635ef5cccf06bd854487
Figo Barosky <figobarosky1@gmail.com>
2026-06-05 04:23:22 +0700

    week14: (task) refactor OrderProcessor applying SRP and DIP

--- src/oop_108111_FigoBarosky/week14/EcommerceTask.kt
+++ src/oop_108111_FigoBarosky/week14/EcommerceTask.kt
@@ -2,26 +2,53 @@ package oop_108111_FigoBarosky.week14
 
 import java.io.File
 
-class BadOrderProcessor {
+interface OrderRepository {
+    fun saveOrder(data: String)
+}
 
-    // VIOLATION: Hardcoded File I/O (DIP), Melakukan kalkulasi + I/O + Notifikasi sekaligus
+class CsvOrderRepository : OrderRepository {
+
     private val file = File("orders.csv")
 
-    fun processOrder(itemName: String, basePrice: Double, customerType: String) {
+    override fun saveOrder(data: String) {
+        file.printWriter().use {
+            it.println(data)
+        }
+    }
+}
 
-        // VIOLATION: Kaku jika ada tipe customer/diskon baru di masa depan (OCP)
+interface NotificationService {
+    fun sendNotification(message: String)
+}
+
+class EmailNotifier : NotificationService {
+
+    override fun sendNotification(message: String) {
+        println("EMAIL: $message")
+    }
+}
+
+class SafeOrderProcessor(
+    private val repo: OrderRepository,
+    private val notifier: NotificationService
+) {
+
+    fun processOrder(
+        itemName: String,
+        basePrice: Double,
+        customerType: String
+    ) {
+
         val finalPrice = when (customerType) {
             "REGULAR" -> basePrice
-            "VIP" -> basePrice * 0.90 // Diskon 10%
+            "VIP" -> basePrice * 0.90
             else -> basePrice
         }
 
-        println("Memproses pesanan $itemName seharga $finalPrice")
+        repo.saveOrder("$itemName,$finalPrice,$customerType")
 
-        // VIOLATION SRP/DIP: Menulis file langsung di class bisnis
-        file.appendText("$itemName,$finalPrice,$customerType\n")
-
-        // VIOLATION SRP/DIP: Notifikasi terikat kuat dengan sistem order
-        println("Email terkirim: Pesanan $itemName Anda telah dikonfirmasi!")
+        notifier.sendNotification(
+            "Pesanan $itemName telah dikonfirmasi"
+        )
     }
 }
